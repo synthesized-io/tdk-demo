fe780c0571be977f6184cbf115f2866dd3045010
Ivan Ponomarev <iponomarev@mail.ru>
2023-03-21 08:44:53 +0200

    reduce the number of generated records

--- test_tdk.py
+++ test_tdk.py
@@ -1,15 +1,23 @@
-def test_input_address_isempty(host):
-    cmd = host.run('docker-compose exec input_db bash -c "psql -U postgres -t -c \'SELECT COUNT(*) FROM address\'"')
-    assert int(cmd.stdout) == 0
-  
-def test_input_actor_isempty(host):
-    cmd = host.run('docker-compose exec input_db bash -c "psql -U postgres -t -c \'SELECT COUNT(*) FROM actor\'"')
-    assert int(cmd.stdout) == 0
+import pytest
 
-def test_output_address_is_not_empty(host):
-    cmd = host.run('docker-compose exec output_db bash -c "psql -U postgres -t -c \'SELECT COUNT(*) FROM address\'"')
-    assert int(cmd.stdout) == 10000
+@pytest.mark.parametrize("table", [
+    "address",
+    "actor", 
+    "country",
+    "city",
+    "store"
+])
+def test_input_db_isempty(host, table):
+    cmd = host.run('docker-compose exec input_db bash -c "psql -U postgres -t -c \'SELECT COUNT(*) FROM %s\'"' % table)
+    assert int(cmd.stdout) == 0
   
-def test_output_actor_is_not_empty(host):
-    cmd = host.run('docker-compose exec output_db bash -c "psql -U postgres -t -c \'SELECT COUNT(*) FROM actor\'"')
-    assert int(cmd.stdout) == 10000
+@pytest.mark.parametrize("table,expected",   [
+    ("address",1000),
+    ("actor", 1000), 
+    ("country", 20),
+    ("city", 100),
+    ("store", 500)
+])
+def test_output_db_is_not_empty(host, table, expected):
+    cmd = host.run('docker-compose exec output_db bash -c "psql -U postgres -t -c \'SELECT COUNT(*) FROM %s\'"' % table)
+    assert int(cmd.stdout) == expected
