cf22d57fad4db5c743cb0ee32515436eba53c65e
mgramin <mgramin@gmail.com>
2023-04-23 22:27:35 +0300

    decrease rows count

--- test_tdk.py
+++ test_tdk.py
@@ -2,7 +2,7 @@ import pytest
 
 @pytest.mark.parametrize("table", [
     "address",
-    "actor", 
+    "actor",
     "country",
     "city",
     "store"
@@ -10,13 +10,13 @@ import pytest
 def test_input_db_isempty(host, table):
     cmd = host.run('docker-compose exec -T input_db bash -c "psql -U postgres -t -c \'SELECT COUNT(*) FROM %s\'"' % table)
     assert int(cmd.stdout) == 0
-  
+
 @pytest.mark.parametrize("table,expected",   [
     ("address",1000),
-    ("actor", 1000), 
-    ("country", 20),
-    ("city", 100),
-    ("store", 500)
+    ("actor", 1000),
+    ("country", 200),
+    ("city", 500),
+    ("store", 1000)
 ])
 def test_output_db_is_not_empty(host, table, expected):
     cmd = host.run('docker-compose exec -T output_db bash -c "psql -U postgres -t -c \'SELECT COUNT(*) FROM %s\'"' % table)
