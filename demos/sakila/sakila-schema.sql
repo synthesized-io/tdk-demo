8679957297bb974c19c2c917c84f3ae142f16a37
mgramin <mgramin@gmail.com>
2023-07-27 13:00:23 +0300

    Update sakila demo and add demo without tdk

--- demos/sakila/sakila-schema.sql
+++ demos/sakila/sakila-schema.sql
@@ -64,11 +64,10 @@ CREATE TABLE address (
   phone VARCHAR(20) NOT NULL,
   -- Add GEOMETRY column for MySQL 5.7.5 and higher
   -- Also include SRID attribute for MySQL 8.0.3 and higher
-  /*!50705 location GEOMETRY */ /*!80003 SRID 0 */ /*!50705 NOT NULL,*/
+  /*!50705 location GEOMETRY */ /*!80003 SRID 0 */ /*!50705 ,*/
   last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY  (address_id),
   KEY idx_fk_city_id (city_id),
-  /*!50705 SPATIAL KEY `idx_location` (location),*/
   CONSTRAINT `fk_address_city` FOREIGN KEY (city_id) REFERENCES city (city_id) ON DELETE RESTRICT ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -210,30 +209,30 @@ SET @@default_storage_engine = @old_default_storage_engine;
 -- Triggers for loading film_text from film
 --
 
-DELIMITER ;;
-CREATE TRIGGER `ins_film` AFTER INSERT ON `film` FOR EACH ROW BEGIN
-    INSERT INTO film_text (film_id, title, description)
-        VALUES (new.film_id, new.title, new.description);
-  END;;
+-- DELIMITER ;;
+-- CREATE TRIGGER `ins_film` AFTER INSERT ON `film` FOR EACH ROW BEGIN
+--     INSERT INTO film_text (film_id, title, description)
+--         VALUES (new.film_id, new.title, new.description);
+--   END;;
 
 
-CREATE TRIGGER `upd_film` AFTER UPDATE ON `film` FOR EACH ROW BEGIN
-    IF (old.title != new.title) OR (old.description != new.description) OR (old.film_id != new.film_id)
-    THEN
-        UPDATE film_text
-            SET title=new.title,
-                description=new.description,
-                film_id=new.film_id
-        WHERE film_id=old.film_id;
-    END IF;
-  END;;
+-- CREATE TRIGGER `upd_film` AFTER UPDATE ON `film` FOR EACH ROW BEGIN
+--     IF (old.title != new.title) OR (old.description != new.description) OR (old.film_id != new.film_id)
+--     THEN
+--         UPDATE film_text
+--             SET title=new.title,
+--                 description=new.description,
+--                 film_id=new.film_id
+--         WHERE film_id=old.film_id;
+--     END IF;
+--   END;;
 
 
-CREATE TRIGGER `del_film` AFTER DELETE ON `film` FOR EACH ROW BEGIN
-    DELETE FROM film_text WHERE film_id = old.film_id;
-  END;;
+-- CREATE TRIGGER `del_film` AFTER DELETE ON `film` FOR EACH ROW BEGIN
+--     DELETE FROM film_text WHERE film_id = old.film_id;
+--   END;;
 
-DELIMITER ;
+-- DELIMITER ;
 
 --
 -- Table structure for table `inventory`
