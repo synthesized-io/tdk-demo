91712b64f58c17dc48aced66829a1a95304e53b2
mgramin <mgramin@gmail.com>
2023-07-31 18:12:55 +0300

    Add generation screencast

--- postgres/record_demo.sh
+++ postgres/record_demo.sh
@@ -3,6 +3,11 @@ export SPIN_DATABASES_UP="docker-compose -f docker-compose.yaml run databases"
 asciinema rec -c "./run_demo.sh" --overwrite generation_from_scratch.cast
 asciinema-agg generation_from_scratch.cast generation_from_scratch.gif
 
+export CONFIG_FILE=config_generation.tdk.yaml
+export SPIN_DATABASES_UP="docker-compose -f docker-compose.yaml -f docker-compose-input-db.yaml run databases"
+asciinema rec -c "./run_demo.sh" --overwrite generation.cast
+asciinema-agg generation.cast generation.gif
+
 export CONFIG_FILE=config_masking.tdk.yaml
 export SPIN_DATABASES_UP="docker-compose -f docker-compose.yaml -f docker-compose-input-db.yaml run databases"
 asciinema rec -c "./run_demo.sh" --overwrite masking.cast
