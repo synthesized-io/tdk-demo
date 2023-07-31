6e0c3562503ec5588156cb01800067e755a559f3
mgramin <mgramin@gmail.com>
2023-07-31 18:04:07 +0300

    Update screencasts

--- postgres/run_demo.sh
+++ postgres/run_demo.sh
@@ -6,18 +6,21 @@ PROMPT_TIMEOUT=0
 
 clear
 
-pe "export CONFIG_FILE=${CONFIG_FILE}"
-
-# pe "docker-compose -f docker-compose.yaml -f docker-compose-input-db.yaml run tdk"
+pe "# Start databases ..."
+pe "${SPIN_DATABASES_UP}"
 # pe "docker-compose -f docker-compose.yaml run tdk"
-docker-compose up -d input_db output_db &> /dev/null
+# docker-compose up -d input_db output_db &> /dev/null
 
+pe "# Check the ORIGINAL database with control sql-query ..."
+pe "usql pg://postgres:postgres@localhost:6000/postgres -f control_query.sql"
+
+pe "# Run the TDK transformation ..."
+pe "export CONFIG_FILE=${CONFIG_FILE}"
 pe "docker-compose -f docker-compose.yaml run tdk"
 
-pe "# check the original database with control sql-query..."
-pe "usql pg://postgres:postgres@localhost:6000/postgres -f control_query.sql"
-pe "# check the original database with control sql-query..."
+pe "# Check the RESULTED database with control sql-query ..."
 pe "usql pg://postgres:postgres@localhost:6001/postgres -f control_query.sql"
+
 p ""
 
 docker-compose down &> /dev/null
