47fff22e6311a7c80ea5533aac5f277e9c379e98
mgramin <mgramin@gmail.com>
2023-08-01 21:07:14 +0300

    Update screencasts

--- mysql/run_demo.sh
+++ mysql/run_demo.sh
@@ -10,14 +10,14 @@ pe "# Start databases ..."
 pe "${SPIN_DATABASES_UP}"
 
 pe "# Check the ORIGINAL database with control sql-query ..."
-pe "usql mysql://root:admin@localhost:6000/sakila -f control_query.sql"
+pe "usql -q mysql://root:admin@localhost:6000/sakila -f control_query.sql"
 
 pe "# Run the TDK transformation ..."
 pe "export CONFIG_FILE=${CONFIG_FILE}"
 pe "docker-compose -f docker-compose.yaml run tdk"
 
 pe "# Check the RESULTED database with control sql-query ..."
-pe "usql mysql://root:admin@localhost:6001/sakila -f control_query.sql"
+pe "usql -q mysql://root:admin@localhost:6001/sakila -f control_query.sql"
 
 docker-compose down &> /dev/null
 
