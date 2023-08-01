efa0c6d733a1340ce491cdd578c7b33725f2b9be
mgramin <mgramin@gmail.com>
2023-08-01 15:57:41 +0300

    Add timeout to the end of the show

--- postgres/run_demo.sh
+++ postgres/run_demo.sh
@@ -19,6 +19,9 @@ pe "docker-compose -f docker-compose.yaml run tdk"
 pe "# Check the RESULTED database with control sql-query ..."
 pe "usql pg://postgres:postgres@localhost:6001/postgres -f control_query.sql"
 
+PROMPT_TIMEOUT=5
+wait
+
 p ""
 
 docker-compose down &> /dev/null
