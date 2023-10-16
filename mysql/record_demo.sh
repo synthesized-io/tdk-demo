9c9e2ddbaa49ede641370cd37c12f2eec66a3a6f
mgramin <mgramin@gmail.com>
2023-10-16 14:13:21 +0300

    Update casts

--- mysql/record_demo.sh
+++ mysql/record_demo.sh
@@ -1,9 +1,9 @@
 export CONFIG_FILE=config_masking.tdk.yaml
-export SPIN_DATABASES_UP="docker-compose -f docker-compose.yaml -f docker-compose-input-db.yaml run databases"
+export SPIN_DATABASES_UP="docker-compose -f docker-compose.yaml -f docker-compose-input-db.yaml run tdk"
 asciinema rec -c "./run_demo.sh" --idle-time-limit=1 --overwrite masking.cast
 asciinema-agg --no-loop masking.cast masking.gif
 
 export CONFIG_FILE=config_subsetting.tdk.yaml
-export SPIN_DATABASES_UP="docker-compose -f docker-compose.yaml -f docker-compose-input-db.yaml run databases"
+export SPIN_DATABASES_UP="docker-compose -f docker-compose.yaml -f docker-compose-input-db.yaml run tdk"
 asciinema rec -c "./run_demo.sh" --idle-time-limit=1 --overwrite subsetting.cast
 asciinema-agg --no-loop subsetting.cast subsetting.gif
