f15290b76b8c644f2ffe97875ae4d38e6063ce79
mgramin <mgramin@gmail.com>
2023-12-25 18:57:57 +0300

    Update

--- postgres/run_demo.sh
+++ postgres/run_demo.sh
@@ -10,10 +10,10 @@ pe "# Run the TDK transformation ..."
 pe "export CONFIG_FILE=${CONFIG_FILE}"
 pe "${SPIN_DATABASES_UP} &> /dev/null"
 
-pe "# Check the ORIGINAL database with control sql-query ..."
+pe "${MESSAGE_BEFORE}"
 pe "usql -q pg://postgres:postgres@localhost:6000/postgres -f control_query.sql"
 
-pe "# Check the RESULTED database with control sql-query ..."
+pe "${MESSAGE_AFTER}"
 pe "usql -q pg://postgres:postgres@localhost:6001/postgres -f control_query.sql"
 
 docker-compose down &> /dev/null
