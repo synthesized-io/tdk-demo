1d04756c46790cff77fceac57d67e3dfa594f403
mgramin <mgramin@gmail.com>
2023-12-26 13:41:08 +0300

    Update

--- postgres/run_demo.sh
+++ postgres/run_demo.sh
@@ -4,14 +4,16 @@
 DEMO_PROMPT="${GREEN}➜ ${CYAN}\W ${COLOR_RESET}"
 PROMPT_TIMEOUT=0
 
-clear
+${SPIN_DATABASES_UP} databases &> /dev/null
 
-pe "# Run the TDK transformation ..."
-pe "export CONFIG_FILE=${CONFIG_FILE}"
-pe "${SPIN_DATABASES_UP} &> /dev/null"
+clear
 
 pe "${MESSAGE_BEFORE}"
 pe "usql -q pg://postgres:postgres@localhost:6000/postgres -f control_query.sql"
+
+pe "# Run the TDK transformation ..."
+pe "export CONFIG_FILE=${CONFIG_FILE}"
+pe "${SPIN_DATABASES_UP} tdk &> /dev/null"
 
 pe "${MESSAGE_AFTER}"
 pe "usql -q pg://postgres:postgres@localhost:6001/postgres -f control_query.sql"
