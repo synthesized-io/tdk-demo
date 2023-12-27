2bd29d76cc696cbfb876514c47ad18af89a2b6a0
mgramin <mgramin@gmail.com>
2023-12-27 14:41:26 +0300

    Update

--- postgres/record_demo.sh
+++ postgres/record_demo.sh
@@ -20,7 +20,7 @@ export CONFIG_FILE=config_masking.tdk.yaml
 export SPIN_DATABASES_UP="docker-compose run"
 export MESSAGE_BEFORE="# Typically, databases contain sensitive data like names, emails, financial information and so on ..."
 export MESSAGE_START_TRANSFORMATION="# In this case, you can run Synthesized TDK in the masking mode ..."
-export MESSAGE_AFTER="# Hence, you've obtained realistic data, preserving original properties without any private information ..."
+export MESSAGE_AFTER="# Hence, you've obtained realistic data, preserving original properties, but without any private information ..."
 asciinema rec --rows=22 -c "./run_demo.sh" --idle-time-limit=1 --overwrite masking.cast
 docker run --rm -it -u $(id -u):$(id -g) -v $PWD:/data agg masking.cast masking.gif --font-size 26 --no-loop
 
