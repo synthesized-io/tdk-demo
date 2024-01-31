87f0d384545be17648b7fc6a4c596541cfc08b2e
Vitaly Ivanov <vitaly@synthesized.io>
2024-01-31 15:35:32 +0000

    TDK v1.43.0 (#39)
    * Disable trigger creation for MySQL
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Maksim Gramin <mgramin@gmail.com>

--- mysql/sakila-schema.sql
+++ mysql/sakila-schema.sql
@@ -209,30 +209,30 @@ SET @@default_storage_engine = @old_default_storage_engine;
 -- Triggers for loading film_text from film
 --
 
-DELIMITER ;;
-CREATE TRIGGER `ins_film` AFTER INSERT ON `film` FOR EACH ROW BEGIN
-    INSERT INTO film_text (film_id, title, description)
-        VALUES (new.film_id, new.title, new.description);
-  END;;
+-- DELIMITER ;;
+-- CREATE TRIGGER `ins_film` AFTER INSERT ON `film` FOR EACH ROW BEGIN
+--     INSERT INTO film_text (film_id, title, description)
+--         VALUES (new.film_id, new.title, new.description);
+--   END;;
 
 
-CREATE TRIGGER `upd_film` AFTER UPDATE ON `film` FOR EACH ROW BEGIN
-    IF (old.title != new.title) OR (old.description != new.description) OR (old.film_id != new.film_id)
-    THEN
-        UPDATE film_text
-            SET title=new.title,
-                description=new.description,
-                film_id=new.film_id
-        WHERE film_id=old.film_id;
-    END IF;
-  END;;
+-- CREATE TRIGGER `upd_film` AFTER UPDATE ON `film` FOR EACH ROW BEGIN
+--     IF (old.title != new.title) OR (old.description != new.description) OR (old.film_id != new.film_id)
+--     THEN
+--         UPDATE film_text
+--             SET title=new.title,
+--                 description=new.description,
+--                 film_id=new.film_id
+--         WHERE film_id=old.film_id;
+--     END IF;
+--   END;;
 
 
-CREATE TRIGGER `del_film` AFTER DELETE ON `film` FOR EACH ROW BEGIN
-    DELETE FROM film_text WHERE film_id = old.film_id;
-  END;;
+-- CREATE TRIGGER `del_film` AFTER DELETE ON `film` FOR EACH ROW BEGIN
+--     DELETE FROM film_text WHERE film_id = old.film_id;
+--   END;;
 
-DELIMITER ;
+-- DELIMITER ;
 
 --
 -- Table structure for table `inventory`
