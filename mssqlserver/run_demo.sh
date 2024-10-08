ae8e3b53267a03aaa85530b44784b2617cb129e2
mgramin <mgramin@gmail.com>
2024-10-08 18:57:24 +0300

    test

--- mssqlserver/run_demo.sh
+++ mssqlserver/run_demo.sh
@@ -9,14 +9,14 @@ ${SPIN_DATABASES_UP} databases &> /dev/null
 clear
 
 pe "${MESSAGE_BEFORE}"
-pe "usql ms://sa:Secret_password_1@localhost:6000/northwind -f control_query.sql"
+pe "usql -q ms://sa:Secret_password_1@localhost:6000/northwind -f control_query.sql"
 
 pe "${MESSAGE_START_TRANSFORMATION}"
 pe "export CONFIG_FILE=${CONFIG_FILE}"
 pe "${SPIN_DATABASES_UP} tdk &> /dev/null"
 
 pe "${MESSAGE_AFTER}"
-pe "usql ms://sa:Secret_password_1@localhost:6001/northwind -f control_query.sql"
+pe "usql -q ms://sa:Secret_password_1@localhost:6001/northwind -f control_query.sql"
 
 docker compose down &> /dev/null
 
